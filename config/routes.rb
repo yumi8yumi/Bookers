401bc834e3de295552e1823560593d5bb5355479
yumi8yumi <yumikokaho@gmail.com>
2022-04-10 02:03:46 +0000

    [add]ルートパス設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   
+
   get '/top' => 'homes#top'
+  root to:'homes#top'
+  patch 'books/:id' => 'books#update', as: 'update_book'
   resources:books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
