cc02f0d9f074cff82285336e42585089945a2076
yumi8yumi <yumikokaho@gmail.com>
2022-04-12 12:45:46 +0000

    [add]validates

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,11 +2,16 @@ class BooksController < ApplicationController
   def new
     @book=Book.new
   end
-  
+
   def create
     @book=Book.new(book_params)
-    @book.save
+    @books=Book.all
+   if @book.save
     redirect_to book_path(@book.id)
+   else
+    # redirect_to books_path
+     render "index" 
+   end
   end
 
   def index
@@ -21,22 +26,22 @@ class BooksController < ApplicationController
   def edit
     @book=Book.find(params[:id])
   end
-  
+
   def update
     @book=Book.find(params[:id])
     @book.update(book_params)
     redirect_to book_path(@book.id)
   end
-  
+
   def destroy
     @book=Book.find(params[:id])
     @book.destroy
     redirect_to'/books'
   end
-  
+
   private
   def book_params
     params.require(:book).permit(:title,:body)
   end
-  
+
 end
