5f11025f72cff9fc85e623790d7aa288737d0fd7
yumi8yumi <yumikokaho@gmail.com>
2022-04-13 11:17:41 +0000

    [add]フラッシュメッセージ実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,6 +7,7 @@ class BooksController < ApplicationController
     @book=Book.new(book_params)
     @books=Book.all
    if @book.save
+     flash[:notice]="Book was successfully created."
     redirect_to book_path(@book.id)
    else
     # redirect_to books_path
@@ -31,6 +32,7 @@ class BooksController < ApplicationController
     @book=Book.find(params[:id])
     @book.update(book_params)
     if @book.save
+      flash[:notice] = "Book was successfully updated."
       redirect_to book_path(@book.id)
     else
       render "edit"
